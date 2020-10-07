bef16caa156a98f73c605b191f23660dd6abb76e
Bhaskar Brahma <bhbrahma@microsoft.com>
2020-10-06 17:12:59 -0700

    fixed formatting for packageregistry/utils.go

--- internal/packageregistry/utils.go
+++ internal/packageregistry/utils.go
@@ -2,9 +2,9 @@ package packageregistry
 
 import "github.com/pkg/errors"
 
-func (self CurrentPackageRegistry)Populate(collection VMAppPackageCurrentCollection)(error){
-	for _, v :=  range collection{
-		if _, exists := self[v.ApplicationName]; exists{
+func (self CurrentPackageRegistry) Populate(collection VMAppPackageCurrentCollection) (error) {
+	for _, v := range collection {
+		if _, exists := self[v.ApplicationName]; exists {
 			return errors.Errorf("Duplicate application name %s detected in application registry", v.ApplicationName)
 		}
 		self[v.ApplicationName] = v
@@ -12,17 +12,17 @@ func (self CurrentPackageRegistry)Populate(collection VMAppPackageCurrentCollect
 	return nil
 }
 
-func (self CurrentPackageRegistry)GetPackageCollection()(collection VMAppPackageCurrentCollection){
+func (self CurrentPackageRegistry) GetPackageCollection() (collection VMAppPackageCurrentCollection) {
 	collection = make(VMAppPackageCurrentCollection, 0)
-	for _, value := range self{
+	for _, value := range self {
 		collection = append(collection, value)
 	}
 	return collection
 }
 
-func (self DesiredPackageRegistry)Populate(collection VMAppPackageIncomingCollection)(error){
-	for _, v :=  range collection{
-		if _, exists := self[v.ApplicationName]; exists{
+func (self DesiredPackageRegistry) Populate(collection VMAppPackageIncomingCollection) (error) {
+	for _, v := range collection {
+		if _, exists := self[v.ApplicationName]; exists {
 			return errors.Errorf("Duplicate application name %s detected in application registry", v.ApplicationName)
 		}
 		self[v.ApplicationName] = v
@@ -30,25 +30,25 @@ func (self DesiredPackageRegistry)Populate(collection VMAppPackageIncomingCollec
 	return nil
 }
 
-func (self DesiredPackageRegistry)GetPackageCollection()(collection VMAppPackageIncomingCollection){
+func (self DesiredPackageRegistry) GetPackageCollection() (collection VMAppPackageIncomingCollection) {
 	collection = make(VMAppPackageIncomingCollection, 0)
-	for _, value := range self{
+	for _, value := range self {
 		collection = append(collection, value)
 	}
 	return collection
 }
 
-func VMAppPackageIncomingToVmAppPackageCurrent(incoming *VMAppPackageIncoming)(current *VMAppPackageCurrent){
+func VMAppPackageIncomingToVmAppPackageCurrent(incoming *VMAppPackageIncoming) (current *VMAppPackageCurrent) {
 	current = &VMAppPackageCurrent{
-		ApplicationName:incoming.ApplicationName,
-		PackageLocation:incoming.PackageLocation,
-		ConfigurationLocation:incoming.ConfigurationLocation,
-		Version:incoming.Version,
-		InstallCommand:incoming.InstallCommand,
-		RemoveCommand:incoming.RemoveCommand,
-		UpdateCommand:incoming.UpdateCommand,
-		DirectDownloadOnly:incoming.DirectDownloadOnly,
-		OngoingOperation:NoAction,
+		ApplicationName:       incoming.ApplicationName,
+		PackageLocation:       incoming.PackageLocation,
+		ConfigurationLocation: incoming.ConfigurationLocation,
+		Version:               incoming.Version,
+		InstallCommand:        incoming.InstallCommand,
+		RemoveCommand:         incoming.RemoveCommand,
+		UpdateCommand:         incoming.UpdateCommand,
+		DirectDownloadOnly:    incoming.DirectDownloadOnly,
+		OngoingOperation:      NoAction,
 	}
 	return current
 }
