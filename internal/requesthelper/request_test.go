23854d9248b3fa4873b100f9a5b3e94b25ec8fc0
Joseph Calev <jcalev@microsoft.com>
2020-12-10 09:57:07 -0800

    Request and retry

---
+++ internal/requesthelper/request_test.go
@@ -0,0 +1,135 @@
+package requesthelper_test
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/Azure/VMApplication-Extension/internal/requesthelper"
+	"github.com/ahmetalpbalkan/go-httpbin"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	testRequestTimeout = 2 * time.Second
+)
+
+type testUrlRequest struct{ url string }
+type badDownloader struct{ calls int }
+
+func (b *badDownloader) GetRequest() (*http.Request, error) {
+	b.calls++
+	return nil, errors.New("expected error")
+}
+
+func NewTestURLRequest(url string) requesthelper.RequestFactory {
+	return testUrlRequest{url}
+}
+
+func (u testUrlRequest) GetRequest() (*http.Request, error) {
+	return http.NewRequest("GET", u.url, nil)
+}
+
+func TestMakeRequest_wrapsGetRequestError(t *testing.T) {
+	rm := requesthelper.GetRequestManager(new(badDownloader), testRequestTimeout)
+	_, err := rm.MakeRequest()
+	require.NotNil(t, err)
+	require.EqualError(t, err, "failed to create http request: expected error")
+}
+
+func TestMakeRequest_wrapsHTTPError(t *testing.T) {
+	rm := requesthelper.GetRequestManager(NewTestURLRequest("bad url"), testRequestTimeout)
+	_, err := rm.MakeRequest()
+	require.NotNil(t, err)
+	require.Contains(t, err.Error(), "http request failed:")
+}
+
+func TestMakeRequest_requestTimeout(t *testing.T) {
+	srv := httptest.NewServer(httpbin.GetMux())
+	defer srv.Close()
+
+	rm := requesthelper.GetRequestManager(
+		NewTestURLRequest(fmt.Sprintf("%s/delay/%d", srv.URL, 3)),
+		testRequestTimeout)
+	_, err := rm.MakeRequest()
+	require.NotNil(t, err, "did not fail")
+	require.Contains(t, err.Error(), "Timeout")
+
+	rm = requesthelper.GetRequestManager(
+		NewTestURLRequest(fmt.Sprintf("%s/delay/%d", srv.URL, 1)),
+		testRequestTimeout)
+	resp, err := rm.MakeRequest()
+	require.Nil(t, err)
+	defer resp.Body.Close()
+	require.NotNil(t, resp.Body)
+}
+
+func TestMakeRequest_badStatusCodeFails(t *testing.T) {
+	srv := httptest.NewServer(httpbin.GetMux())
+	defer srv.Close()
+
+	for _, code := range []int{
+		http.StatusNotFound,
+		http.StatusForbidden,
+		http.StatusInternalServerError,
+		http.StatusBadGateway,
+		http.StatusBadRequest,
+		http.StatusUnauthorized,
+	} {
+		rm := requesthelper.GetRequestManager(
+			NewTestURLRequest(fmt.Sprintf("%s/status/%d", srv.URL, code)),
+			testRequestTimeout)
+		_, err := rm.MakeRequest()
+		require.NotNil(t, err, "not failed for code:%d", code)
+		require.Contains(t, err.Error(), "unexpected status code", "wrong message for code %d", code)
+	}
+}
+
+func TestMakeRequest_statusOKSucceeds(t *testing.T) {
+	srv := httptest.NewServer(httpbin.GetMux())
+	defer srv.Close()
+
+	rm := requesthelper.GetRequestManager(NewTestURLRequest(srv.URL+"/status/200"), testRequestTimeout)
+	resp, err := rm.MakeRequest()
+	require.Nil(t, err)
+	defer resp.Body.Close()
+	require.NotNil(t, resp.Body)
+}
+
+func TestMakeRequest_statusPartialResultsSucceeds(t *testing.T) {
+	srv := httptest.NewServer(httpbin.GetMux())
+	defer srv.Close()
+
+	rm := requesthelper.GetRequestManager(NewTestURLRequest(srv.URL+"/status/206"), testRequestTimeout)
+	resp, err := rm.MakeRequest()
+	require.Nil(t, err)
+	defer resp.Body.Close()
+	require.NotNil(t, resp.Body)
+}
+
+func TestMakeRequest_retrievesBody(t *testing.T) {
+	srv := httptest.NewServer(httpbin.GetMux())
+	defer srv.Close()
+
+	rm := requesthelper.GetRequestManager(NewTestURLRequest(srv.URL+"/bytes/65536"), testRequestTimeout)
+	resp, err := rm.MakeRequest()
+	require.Nil(t, err)
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	require.Nil(t, err)
+	require.EqualValues(t, 65536, len(b))
+}
+
+func TestMakeRequest_bodyClosesWithoutError(t *testing.T) {
+	srv := httptest.NewServer(httpbin.GetMux())
+	defer srv.Close()
+
+	rm := requesthelper.GetRequestManager(NewTestURLRequest(srv.URL+"/get"), testRequestTimeout)
+	resp, err := rm.MakeRequest()
+	require.Nil(t, err)
+	require.Nil(t, resp.Body.Close(), "body should close fine")
+}
