e07864a85525b9f826ab4d1e51f2dec31be4c8f4
norakoiralamsft <88748701+norakoiralamsft@users.noreply.github.com>
2021-10-04 10:41:20 -0700

    Adding imports to fix build fail for linux

--- main/update_linux.go
+++ main/update_linux.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	vmextensionhelper "github.com/Azure/azure-extension-platform/vmextension"
+	"github.com/pkg/errors"
+)
+
 func vmAppUpdateCallback(ext *vmextensionhelper.VMExtension) error {
 	//no-op function
 	return nil 
