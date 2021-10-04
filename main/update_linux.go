f90efd5db2301bf0d0f17460b9d75c2a5ecbf098
norakoiralamsft <88748701+norakoiralamsft@users.noreply.github.com>
2021-10-04 10:43:43 -0700

    Removing errors import, unused

--- main/update_linux.go
+++ main/update_linux.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	vmextensionhelper "github.com/Azure/azure-extension-platform/vmextension"
-	"github.com/pkg/errors"
 )
 
 func vmAppUpdateCallback(ext *vmextensionhelper.VMExtension) error {
