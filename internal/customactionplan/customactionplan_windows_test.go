4246fc1574e3c07f956d4d1c2b2a6a6924d959a1
Nora Koirala <norakoirala@microsoft.com>
2022-01-20 09:58:58 -0800

    fix cleanup err

--- internal/customactionplan/customactionplan_windows_test.go
+++ internal/customactionplan/customactionplan_windows_test.go
@@ -138,6 +138,7 @@ func TestCommandExecutorCanHandleProcessBeingKilled(t *testing.T) {
 		assertTickCountFileCorrect(t, action[0].Actions[0].TickCount)
 		assert.EqualValues(t, (*packageOperationResults)[0], actionplan.PackageOperationResult{Result: actionplan.Success, Operation: "action1", AppVersion: "1.0", PackageName: newApp.ApplicationName, Timestamp: "20210604T155300Z"})
 	} else {
+		cleanupTest()
 		currentDirAbsolutePath, err := filepath.Abs("")
 		assert.NoError(t, err, "should be able to get absolute path")
 		transcriptFile := path.Join(currentDirAbsolutePath, testdir, "transcript.txt")
