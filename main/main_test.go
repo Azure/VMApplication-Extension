9b692c5185d5f7e200eb36cbe5cb04447802c312
Joseph Calev <jcalev@microsoft.com>
2022-04-27 16:06:31 -0700

    Fixes build issues

--- main/main_test.go
+++ main/main_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/Azure/VMApplication-Extension/internal/extdeserialization"
 	"errors"
 	"fmt"
 	"io/ioutil"
@@ -11,6 +10,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/Azure/VMApplication-Extension/internal/extdeserialization"
 	"github.com/Azure/VMApplication-Extension/internal/hostgacommunicator"
 	"github.com/Azure/VMApplication-Extension/internal/packageregistry"
 	"github.com/Azure/azure-extension-platform/pkg/constants"
@@ -111,7 +111,7 @@ func Test_getVMPackageData_noApplications(t *testing.T) {
 func Test_getVMPackageData_valid(t *testing.T) {
 	order := 1
 	vmApplications := []extdeserialization.VmAppSetting{
-		extdeserialization.VmAppSetting{
+		{
 			ApplicationName: "iggy",
 			Order:           &order,
 		},
@@ -127,12 +127,12 @@ func Test_getVMPackageData_valid(t *testing.T) {
 func Test_getVMAppProtectedSettings_valid(t *testing.T) {
 	order := 1
 	actions := extdeserialization.ActionSetting{
-		ActionName: "logging",
+		ActionName:   "logging",
 		ActionScript: "echo %CustomAction_blobURL%",
-		Timestamp: "20210604T155300Z",
+		Timestamp:    "20210604T155300Z",
 		Parameters: []extdeserialization.ActionParameter{
 			{
-				ParameterName: "blobURL",
+				ParameterName:  "blobURL",
 				ParameterValue: "myaccount.blob.core.windows.net",
 			},
 		},
@@ -140,13 +140,12 @@ func Test_getVMAppProtectedSettings_valid(t *testing.T) {
 	}
 	appSettings := extdeserialization.VmAppSetting{
 		ApplicationName: "iggy",
-		Order: &order,
-		Actions: []*extdeserialization.ActionSetting{&actions},
-
+		Order:           &order,
+		Actions:         []*extdeserialization.ActionSetting{&actions},
 	}
 	vmAppProtectedSettings := VmAppProtectedSettings{&appSettings}
-	testSettings := handlersettings.HandlerSettings {
-		PublicSettings: "{}",
+	testSettings := handlersettings.HandlerSettings{
+		PublicSettings:    "{}",
 		ProtectedSettings: "[{\"name\": \"iggy\", \"order\": 1, \"actions\": [{\"name\": \"logging\",\"script\": \"echo %CustomAction_blobURL%\",\"timestamp\": \"20210604T155300Z\",\"parameters\": [{\"name\": \"blobURL\",\"value\": \"myaccount.blob.core.windows.net\"}],\"tickCount\": 10193113}]}]",
 	}
 
@@ -162,7 +161,7 @@ func Test_getVMAppProtectedSettings_valid(t *testing.T) {
 func Test_getVMPackageData_noVersion(t *testing.T) {
 	order := 1
 	vmApplications := []extdeserialization.VmAppSetting{
-		extdeserialization.VmAppSetting{
+		{
 			ApplicationName: "iggy",
 			Order:           &order,
 		},
@@ -178,17 +177,17 @@ func Test_getVMPackageData_noVersion(t *testing.T) {
 func Test_getVMPackageDataCustomAction_valid(t *testing.T) {
 	order := 1
 	actions := extdeserialization.ActionSetting{
-				ActionName: "Action1",
-				ActionScript: "echo hello",
-				Timestamp: "20210604T155300Z",
-				Parameters: []extdeserialization.ActionParameter{},
-				TickCount: 12346578,
-		}
+		ActionName:   "Action1",
+		ActionScript: "echo hello",
+		Timestamp:    "20210604T155300Z",
+		Parameters:   []extdeserialization.ActionParameter{},
+		TickCount:    12346578,
+	}
 	vmApplications := []extdeserialization.VmAppSetting{
-		extdeserialization.VmAppSetting{
+		{
 			ApplicationName: "iggy",
 			Order:           &order,
-			Actions:		[]*extdeserialization.ActionSetting{&actions},
+			Actions:         []*extdeserialization.ActionSetting{&actions},
 		},
 	}
 
@@ -202,17 +201,17 @@ func Test_getVMPackageDataCustomAction_valid(t *testing.T) {
 func Test_getVMPackageDataCustomAction_CriticalError(t *testing.T) {
 	order := 1
 	actions := extdeserialization.ActionSetting{
-		ActionName: "Action1",
+		ActionName:   "Action1",
 		ActionScript: "echo hello",
-		Timestamp: "20210604T155300Z",
-		Parameters: []extdeserialization.ActionParameter{},
-		TickCount: 12346578,
+		Timestamp:    "20210604T155300Z",
+		Parameters:   []extdeserialization.ActionParameter{},
+		TickCount:    12346578,
 	}
 	vmApplications := []extdeserialization.VmAppSetting{
-		extdeserialization.VmAppSetting{
+		{
 			ApplicationName: "",
-			Order: &order,
-			Actions:		[]*extdeserialization.ActionSetting{&actions},
+			Order:           &order,
+			Actions:         []*extdeserialization.ActionSetting{&actions},
 		},
 	}
 
@@ -226,7 +225,7 @@ func Test_getVMPackageDataCustomAction_CriticalError(t *testing.T) {
 func Test_getVMPackageData_noApplicationName(t *testing.T) {
 	order := 1
 	vmApplications := []extdeserialization.VmAppSetting{
-		extdeserialization.VmAppSetting{
+		{
 			ApplicationName: "",
 			Order:           &order,
 		},
@@ -250,7 +249,7 @@ func Test_main_nothingToProcess(t *testing.T) {
 }
 
 func Test_uninstall_cannotCreatePackageRegistry(t *testing.T) {
-	vmApplications := []VmAppSetting{}
+	vmApplications := []extdeserialization.VmAppSetting{}
 	ext := createTestVMExtension(t, vmApplications)
 	hostGaCommunicator := NoopHostGaCommunicator{}
 
@@ -263,7 +262,7 @@ func Test_uninstall_cannotCreatePackageRegistry(t *testing.T) {
 }
 
 func Test_uninstall_cannotReadPackageRegistry(t *testing.T) {
-	vmApplications := []VmAppSetting{}
+	vmApplications := []extdeserialization.VmAppSetting{}
 	ext := createTestVMExtension(t, vmApplications)
 	hostGaCommunicator := NoopHostGaCommunicator{}
 
@@ -278,7 +277,7 @@ func Test_uninstall_cannotReadPackageRegistry(t *testing.T) {
 }
 
 func Test_uninstall_noAppsToUninstall(t *testing.T) {
-	vmApplications := []VmAppSetting{}
+	vmApplications := []extdeserialization.VmAppSetting{}
 	ext := createTestVMExtension(t, vmApplications)
 	hostGaCommunicator := NoopHostGaCommunicator{}
 
@@ -319,7 +318,7 @@ func Test_uninstall_noAppsToUninstall(t *testing.T) {
 }
 
 func Test_uninstall_uninstallApps(t *testing.T) {
-	vmApplications := []VmAppSetting{}
+	vmApplications := []extdeserialization.VmAppSetting{}
 	ext := createTestVMExtension(t, vmApplications)
 	hostGaCommunicator := NoopHostGaCommunicator{}
 
@@ -387,4 +386,3 @@ func createTestVMExtension(t *testing.T, settings interface{}) *vmextension.VMEx
 		ExtensionEvents: eem,
 	}
 }
-
