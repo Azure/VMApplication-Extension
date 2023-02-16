44339627092266363b46f6b24f8b6b50020ee7ad
Bhaskar Brahma <bhbrahma@microsoft.com>
2023-02-15 18:18:45 -0800

    updated to full path for extension launcher

--- launcher/launcher_linux.go
+++ launcher/launcher_linux.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
+	"path/filepath"
+
 	"github.com/Azure/azure-extension-platform/pkg/commandhandler"
 	"github.com/Azure/azure-extension-platform/pkg/logging"
+	"github.com/Azure/azure-extension-platform/pkg/utils"
 )
 
 var commandHandlerToUse = commandhandler.New()
 
 func runExecutableAsIndependentProcess(exeName, args, workingDir, logDir string, el *logging.ExtensionLogger) {
-	commandToExecute := fmt.Sprintf("%s %s", exeName, args)
+	exepath, err := utils.GetCurrentProcessWorkingDir()
+	if err != nil {
+		el.Error("could not determine current process working directory %s", err.Error())
+		return
+	}
+	exefullPathAndName := filepath.Join(exepath, exeName)
+	commandToExecute := fmt.Sprintf("%s %s", exefullPathAndName, args)
 	commandHandlerToUse.Execute(commandToExecute, workingDir, logDir, false, el)
 }
