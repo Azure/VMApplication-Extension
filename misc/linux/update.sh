12ed2ba62a7ac25869cbd2bc236990be131e5567
REDMOND\bhbrahma <bhbrahma@microsoft.com>
2023-02-15 20:41:40 -0800

    update install and update scripts for linux

--- misc/linux/update.sh
+++ misc/linux/update.sh
@@ -1,11 +1,12 @@
-set -e -u -o pipefail
+#!/usr/bin/env bash
+set -euo pipefail
 
 readonly SCRIPT_DIR=$(dirname "$0")
 readonly ARCHITECTURE=$( [[ "$(uname -p)" == "unknown" ]] && echo "$(uname -m)" || echo "$(uname -p)" ) #ternary operator
 
 if [ $ARCHITECTURE == "arm64" ] || [ $ARCHITECTURE == "aarch64" ]; then
-     mv -f "$SCRIPT_DIR/extension-launcher-arm64" "$SCRIPT_DIR/extension-launcher"
-     mv -f "$SCRIPT_DIR/vm-application-manager-arm64" "$SCRIPT_DIR/vm-application-manager"
+    mv -f "$SCRIPT_DIR/extension-launcher-arm64" "$SCRIPT_DIR/extension-launcher"
+    mv -f "$SCRIPT_DIR/vm-application-manager-arm64" "$SCRIPT_DIR/vm-application-manager"
 else
     rm -f "$SCRIPT_DIR/extension-launcher-arm64"
     rm -f "$SCRIPT_DIR/vm-application-manager-arm64"
