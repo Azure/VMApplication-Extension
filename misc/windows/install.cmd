5343bd86976ac6fb50a7c55e6a9950c465d22efe
Mayank Daruka <mayankdarukamsft@gmail.com>
2024-04-04 18:32:39 -0500

    Initial commit to include exe files in zip instead of moving them in install.cmd

--- misc/windows/install.cmd
+++ misc/windows/install.cmd
@@ -1,8 +1,6 @@
 IF "%PROCESSOR_ARCHITECTURE%"=="ARM64" (
-    copy /Y bin\extension-launcher-arm64 bin\extension-launcher.exe
-    copy /Y bin\vm-application-manager-arm64 bin\vm-application-manager.exe
-) ELSE (
-    copy /Y bin\extension-launcher bin\extension-launcher.exe
-    copy /Y bin\vm-application-manager bin\vm-application-manager.exe
+    copy /Y bin\extension-launcher-arm64.exe bin\extension-launcher.exe
+    copy /Y bin\vm-application-manager-arm64.exe bin\vm-application-manager.exe
 )
+
 bin\vm-application-manager.exe "install"
