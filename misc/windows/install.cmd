a9593bbe848eff198b9217e5e2aea5044a046fd6
Mayank Daruka <mayankdarukamsft@gmail.com>
2024-04-05 11:52:36 -0500

    Update commands to inherit current directory context

--- misc/windows/install.cmd
+++ misc/windows/install.cmd
@@ -1,6 +1,6 @@
 IF "%PROCESSOR_ARCHITECTURE%"=="ARM64" (
-    copy /Y bin\extension-launcher-arm64.exe bin\extension-launcher.exe
-    copy /Y bin\vm-application-manager-arm64.exe bin\vm-application-manager.exe
+    copy /Y %~dp0bin\extension-launcher-arm64.exe %~dp0bin\extension-launcher.exe
+    copy /Y %~dp0bin\vm-application-manager-arm64.exe %~dp0bin\vm-application-manager.exe
 )
 
-bin\vm-application-manager.exe "install"
+%~dp0bin\vm-application-manager.exe "install"
