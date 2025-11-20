de857499efdd0336200f178d6875621cf393778b
Bhaskar Brahma <bhbrahma+microsoft@microsoft.com>
2025-11-19 16:11:10 -0800

    added MIT license header files to all .go files

--- main/main_windows_test.go
+++ main/main_windows_test.go
@@ -1,3 +1,6 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 package main
 
 var cannotCreatePackageRegistryError = "Could not create package registry: The system cannot find the path specified."
