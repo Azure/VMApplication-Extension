ff2ded8c9818c445b92a9ec32bad40a28c0014bc
Bhaskar Brahma <bhbrahma@microsoft.com>
2026-04-07 16:48:01 -0700

    renamed variable

--- launcher/main.go
+++ launcher/main.go
@@ -50,9 +50,9 @@ func main() {
 	}
 
 	// validate ExtensionVersion against the version reported by Guest Agent
-	if guestAgentVersion, err := vmextension.GetGuestAgentEnvironmetVariable(vmextension.GuestAgentEnvVarExtensionVersion); err == nil {
-		if guestAgentVersion != ExtensionVersion {
-			el.Warn("ExtensionVersion mismatch: compile-time ExtensionVersion value '%s' does not match value '%s' in environment variable '%s'", ExtensionVersion, guestAgentVersion, vmextension.GuestAgentEnvVarExtensionVersion)
+	if extensionVersionFromEnv, err := vmextension.GetGuestAgentEnvironmetVariable(vmextension.GuestAgentEnvVarExtensionVersion); err == nil {
+		if extensionVersionFromEnv != ExtensionVersion {
+			el.Warn("ExtensionVersion mismatch: compile-time ExtensionVersion value '%s' does not match value '%s' in environment variable '%s'", ExtensionVersion, extensionVersionFromEnv, vmextension.GuestAgentEnvVarExtensionVersion)
 		}
 	}
 
