1a0976a49c4a3688c02e46282d8273db52c490b9
Haley Bui-Nguyen <hbuinguyen2025>
2026-04-20 14:04:06 -0400

    Wait a minute so that the lock file is cleaned up on Windows

--- internal/packageregistry/packageregistry_test.go
+++ internal/packageregistry/packageregistry_test.go
@@ -172,7 +172,7 @@ func TestOnlyOneInstanceofPackageRegistryCanExist(t *testing.T) {
 	require.NoError(t, err, "operation should not throw error")
 
 	// let things settle down before retrying
-	time.Sleep(2 * time.Second)
+	time.Sleep(60 * time.Second)
 	pkgHndlr2, err = New(nopLog(), &hndlEnv, time.Second)
 	require.NoError(t, err, "operation should not throw error")
 	err = pkgHndlr2.Close()
